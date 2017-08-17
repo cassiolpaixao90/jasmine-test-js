2453f6c34cd216c0cb3584e390e04ef81bc13530
Cássio Paixão <cassiopaixaoti@gmail.com>
2017-08-17 14:16:51 +0000

    test data builder

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -1,30 +1,44 @@
 describe("Consulta", function() {
+    var cassio;
 
-    it("nao deve cobrar nada se a consulta for um retorno", function() {
-        var cassio = new Paciente("Cassio", 28, 72, 1.80);
-        var consulta = new Consulta(cassio, [], true, true);
-
-        expect(consulta.preco()).toEqual(0);
+    beforeEach(function() {
+        cassio = new PacienteBuilder().constroi();
+        
+        //Caso queira mudar os valores das Variaveis
+        //cassio = new PacienteBuilder().comAltura(1.72).constroi();
     });
 
-    it("deve cobrar 25 por cada procedimento comum", function() {
-        var cassio = new Paciente("Cassio", 28, 72, 1.80);
-        var consulta = new Consulta(cassio, ["proc1", "proc2"], false, false);
+    describe("Consultas que são retornos", function() {
 
-        expect(consulta.preco()).toEqual(50);
+        it("nao deve cobrar nada se a consulta for um retorno", function() {
+            var consulta = new Consulta(cassio, [], true, true);
+
+            expect(consulta.preco()).toEqual(0);
+        });
+
     });
 
-    it("deve dobrar o preco da consulta particular", function() {
-        var cassio = new Paciente("cassio", 28, 72, 1.80);
-        var consulta = new Consulta(cassio, ["proc1", "proc2"], true, false);
+    describe("Consultas que são particulares", function() {
 
-        expect(consulta.preco()).toEqual(50 * 2);
+        it("deve dobrar o preco da consulta particular", function() {
+            var consulta = new Consulta(cassio, ["proc1", "proc2"], true, false);
+
+            expect(consulta.preco()).toEqual(50 * 2);
+        });
+
+        it("deve dobrar o preco da consulta particular mesmo com procedimentos especiais", function() {
+            var consulta = new Consulta(cassio, ["raio-x"], true, false);
+
+            expect(consulta.preco()).toEqual(55 * 2);
+        });
     });
 
-     it("deve cobrar preco especifico dependendo do procedimento", function() {
-        var cassio = new Paciente("Cassio", 28, 72, 1.80);
-        var consulta = new Consulta(cassio, ["procedimento-comum", "raio-x", "procedimento-comum2", "gesso"], false, false);
+    describe("Consultas por um convênio", function() {
+        it("deve cobrar preco especifico dependendo do procedimento", function() {
+            var consulta = new Consulta(cassio, ["procedimento-comum", "raio-x", "procedimento-comum2", "gesso"], false, false);
 
-        expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+            expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+        });
     });
+
 });
